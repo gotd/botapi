614a5f4037d80e24aac42db4ccb1b90417cf2057
tdakkota <tanc13@yandex.ru>
2022-01-27 13:27:21 +0300

    chore: drop old api package

--- api/api.go
+++
@@ -1,6 +0,0 @@
-// Package api implements API types.
-//
-// See https://core.telegram.org/bots/api#user for reference.
-package api
-
-// TODO(ernado): generate types
--- api/result.go
+++
@@ -1,6 +0,0 @@
-package api
-
-// Response is result of API call.
-type Response struct {
-	Result interface{} `json:"result"`
-}
