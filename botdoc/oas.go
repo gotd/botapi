83030eb67670e18abe4f9c3bf5c39a608de6119e
tdakkota <tanc13@yandex.ru>
2022-06-21 23:40:09 +0300

    fix(botdoc): do not define Response manually, use ResponseParameters instead

--- botdoc/oas.go
+++ botdoc/oas.go
@@ -272,31 +272,6 @@ Schemas:
 		})
 		addResponse(resultName, "#/components/schemas/"+resultName, "Result of method invocation")
 	}
-	c.Schemas["Response"] = &ogen.Schema{
-		Description: "Contains information about why a request was unsuccessful.",
-		Type:        "object",
-		Properties: ogen.Properties{
-			{
-				Name: "migrate_to_chat_id",
-				Schema: &ogen.Schema{
-					Description: "The group has been migrated to a supergroup with the specified identifier. " +
-						"This number may be greater than 32 bits and some programming languages may have " +
-						"difficulty/silent defects in interpreting it. But it is smaller than 52 bits, " +
-						"so a signed 64 bit integer or double-precision float type are safe for storing " +
-						"this identifier.",
-					Type:   "integer",
-					Format: "int64",
-				},
-			},
-			{
-				Name: "retry_after",
-				Schema: &ogen.Schema{
-					Description: "In case of exceeding flood control, the number of seconds left to wait before the request can be repeated",
-					Type:        "integer",
-				},
-			},
-		},
-	}
 	c.Schemas["Error"] = &ogen.Schema{
 		Type: "object",
 		Required: []string{
@@ -325,7 +300,7 @@ Schemas:
 			{
 				Name: "parameters",
 				Schema: &ogen.Schema{
-					Ref: "#/components/schemas/Response",
+					Ref: "#/components/schemas/ResponseParameters",
 				},
 			},
 		},
