e86b91039025ec76344aed00adfc85d25f3b6aa0
tdakkota <tanc13@yandex.ru>
2022-07-08 15:10:52 +0300

    fix(gotd-bot-oas): make marshaler deterministic

--- cmd/gotd-bot-oas/main.go
+++ cmd/gotd-bot-oas/main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
 	"flag"
 	"fmt"
-	"io"
 	"net/http"
 	"os"
 	"os/signal"
@@ -14,9 +11,25 @@ import (
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/go-faster/errors"
+	"github.com/go-json-experiment/json"
 
 	"github.com/gotd/botapi/botdoc"
 )
+
+func marshalDeterministic(src interface{}) ([]byte, error) {
+	data, err := json.Marshal(src)
+	if err != nil {
+		return nil, err
+	}
+	val := json.RawValue(data)
+	if err := val.Canonicalize(); err != nil {
+		return nil, errors.Wrap(err, "canonicalize")
+	}
+	if err := val.Indent("", "\t"); err != nil {
+		return nil, errors.Wrap(err, "indent")
+	}
+	return val, nil
+}
 
 func run(ctx context.Context) error {
 	var arg struct {
@@ -31,45 +44,33 @@ func run(ctx context.Context) error {
 	if err != nil {
 		return errors.Wrap(err, "req")
 	}
+
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return errors.Wrap(err, "do")
 	}
-
 	defer func() { _ = res.Body.Close() }()
-	switch res.StatusCode {
-	case http.StatusOK: // ok
-	default:
+
+	if res.StatusCode != http.StatusOK {
 		return errors.Errorf("code %d: %s", res.StatusCode, res.Status)
 	}
 
-	data, err := io.ReadAll(res.Body)
-	if err != nil {
-		return errors.Wrap(err, "read")
-	}
-
-	doc, err := goquery.NewDocumentFromReader(bytes.NewReader(data))
+	doc, err := goquery.NewDocumentFromReader(res.Body)
 	if err != nil {
 		return errors.Wrap(err, "parse")
 	}
 
-	api := botdoc.Extract(doc)
-	buf := new(bytes.Buffer)
-	e := json.NewEncoder(buf)
-	e.SetIndent("", "  ")
-	s, err := api.OAS()
+	spec, err := botdoc.Extract(doc).OAS()
 	if err != nil {
 		return errors.Wrap(err, "generate")
 	}
-	if err := e.Encode(s); err != nil {
-		return errors.Wrap(err, "encode")
+
+	data, err := marshalDeterministic(spec)
+	if err != nil {
+		return errors.Wrap(err, "marshal")
 	}
 
-	if err := os.WriteFile(arg.Target, buf.Bytes(), 0o600); err != nil {
-		return errors.Wrap(err, "write")
-	}
-
-	return nil
+	return os.WriteFile(arg.Target, data, 0o600)
 }
 
 func main() {
